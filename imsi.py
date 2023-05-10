84d86350f8b9d66ae17f7fd43eb53c00f728c3ad
shaheed <97809675+shaheedghazi@users.noreply.github.com>
2023-05-11 00:38:15 +0500

    Update imsi.py

--- imsi.py
+++ imsi.py
@@ -1,11 +1,13 @@
 # IMSI Decrypter
 # Author: o3t1w
 
-import pyshark
-from optparse import OptionParser
-import os, sys
 import datetime
+import os
+import re
 import sqlite3
+from optparse import OptionParser
+from scapy.all import *
+from tabulate import tabulate
 
 class ImsiDecrypter:
     sql_conn = None
@@ -24,16 +26,16 @@ class ImsiDecrypter:
 
     def save_data(self):
         date_time = datetime.datetime.now()
-        self.sql_conn.execute('INSERT INTO imsi_data(imsi, tmsi, mcc, mnc, lac, ci, date_time) VALUES ( ?, ?, ?, ?, ?, ?, ?)',(self.imsi, self.tmsi, self.mcc, self.mnc, self.lac, self.ci, date_time))
+        self.sql_conn.execute('INSERT INTO imsi_data(imsi, tmsi, mcc, mnc, lac, ci, date_time) VALUES (?, ?, ?, ?, ?, ?, ?)',(self.imsi, self.tmsi, self.mcc, self.mnc, self.lac, self.ci, date_time))
         self.sql_conn.commit()
 
     def get_data(self):
         self.cur = self.sql_conn.cursor()
-        self.cur.execute('SELECT * FROM imsi_data WHERE imsi=' + self.imsi)
+        self.cur.execute('SELECT * FROM imsi_data WHERE imsi=?', (self.imsi,))
         self.data = self.cur.fetchall()
 
     def update_data(self, id_, tmsi):
-        self.sql_conn.execute('UPDATE imsi_data SET tmsi = ?, date_time = ? WHERE id= ?',(tmsi, id_, datetime.datetime.now()))
+        self.sql_conn.execute('UPDATE imsi_data SET tmsi=?, date_time=? WHERE id=?',(tmsi, datetime.datetime.now(), id_))
         self.sql_conn.commit()
 
     def filter_imsi(self):
@@ -50,43 +52,26 @@ class ImsiDecrypter:
                         self.update_data(data[0],self.tmsi)
             else:
                 self.save_data()
-        
+
         if self.imsi in self.live_db:
             if self.live_db[self.imsi]['tmsi'] != self.tmsi: #Check if tmsi is different than update in live db
                 self.live_db[self.imsi]['tmsi'] = self.tmsi
         else:
             self.id_ += 1
-            self.live_db[self.imsi] = {"id" : self.id_,"tmsi" : self.tmsi, "mcc" : self.mcc, "mnc" : self.mnc}
+            self.live_db[self.imsi] = {"id" : self.id_,"tmsi" : self.tmsi, "mcc" : self.mcc, "mnc" : self.mnc, "lac": self.lac, "ci": self.ci}
         self.output()
 
-    def get_imsi(self, capture):
-        for packet in capture:
-            layer = packet.highest_layer
-            if layer == "GSM_A.CCCH":
-                if packet[4].layer_name == 'gsm_a.ccch':
-                    gsm_a_ccch = packet[4]
-                    if hasattr(gsm_a_ccch, "gsm_a_bssmap_cell_ci"):
-                        self.ci = int(gsm_a_ccch.gsm_a_bssmap_cell_ci, 16)
-                        self.lac = int(gsm_a_ccch.gsm_a_lac, 16)
-                    elif hasattr(gsm_a_ccch, 'e212.imsi'):
-                        self.imsi = gsm_a_ccch.e212_imsi #[-11:-1]
-                        self.mcc = gsm_a_ccch.e212_mcc
-                        self.mnc = gsm_a_ccch.e212_mnc
-                        if hasattr(gsm_a_ccch,'gsm_a_rr_tmsi_ptmsi'):
-                            self.tmsi = gsm_a_ccch.gsm_a_rr_tmsi_ptmsi
-                        elif hasattr(gsm_a_ccch,'gsm_a_tmsi'):
-                            self.tmsi = gsm_a_ccch.gsm_a_tmsi
-                        else:
-                            self.tmsi = ''
-                        if options.imsi == '':
-                            self.filter_imsi()
-                        elif options.imsi == self.imsi:
-                            self.filter_imsi()
-                elif packet[6].layer_name == 'gsm_a.ccch':
-                    gsm_a_ccch = packet[6]
-                    if hasattr(gsm_a_ccch, "gsm_a_bssmap_cell_ci"):
-                        self.ci = int(gsm_a_ccch.gsm_a_bssmap_cell_ci, 16)
-                        self.lac = int(gsm_a_ccch.gsm_a_lac, 16)
+    def validate_imsi(self, imsi):
+        pattern = re.compile("^[0-9]{15}$")
+        if pattern.match(imsi):
+            return True
+        return False
+
+    def validate_tmsi(self, tmsi):
+        pattern = re.compile("^[0-9A-F]{4,8}$")
+        if pattern.match(tmsi):
+            return True
+        return False
 
     def header(self):
         os.system('clear')
@@ -96,33 +81,67 @@ class ImsiDecrypter:
 
         '''
         print ("\033[0;31;48m" + title)
-
     def output(self):
         os.system('clear')
-        print("\033[0;37;48m ID \033[0;31;48m; \033[0;37;48m       IMSI       \033[0;31;48m;     \033[0;37;48mTMSI     \033[0;31;48m;   \033[0;37;48mMCC   \033[0;31;48m;  \033[0;37;48mMNC  \033[0;31;48m; \033[0;37;48m  LAC   \033[0;31;48m; \033[0;37;48m    CI\033[0;31;48m    ;")
-        print ("\033[0;31;48m................................................................................")
+        headers = ["ID", "IMSI", "TMSI", "MCC", "MNC", "LAC", "CI"]
+        table = []
         for imsi in self.live_db:
             data = self.live_db[imsi]
-            print("\033[0;37;48m {:3s}\033[0;31;48m; \033[0;37;48m {:16s} \033[0;31;48m; \033[0;37;48m {:12s}\033[0;31;48m; \033[0;37;48m\033[0;37;48m  {:5s} \033[0;31;48m;\033[0;37;48m   {:4s}\033[0;31;48m; \033[0;37;48m {:5}  \033[0;31;48m; \033[0;37;48m {:6}   \033[0;31;48m;".format(str(data["id"]), imsi, data["tmsi"], data["mcc"], data["mnc"], data["lac"], data["ci"]))
-            print ("\033[0;31;48m................................................................................")
+            row = [data["id"], imsi, data["tmsi"], data["mcc"], data["mnc"], data["lac"], data["ci"]]
+            table.append(row)
+        print(tabulate(table, headers, tablefmt="fancy_grid"))
 
-    def start(self):
-        try:
-            self.header()
-            capture = pyshark.LiveCapture(interface=options.iface, bpf_filter="port {} and not icmp and udp".format(options.port))
-            for packet in capture.sniff_continuously():
-                self.get_imsi(packet)
-                self.output()
-        except KeyboardInterrupt:
-            print("Sniffing stopped")
-
-
-if __name__ == "__main__":
-    parser = OptionParser(usage="%prog: [options]")
-    parser.add_option("-i", "--iface", dest="iface", default="lo", help="Interface (default : lo)")
-    parser.add_option("-p", "--port", dest="port", default="4729", type="int", help="Port (default : 4729)")    
-    parser.add_option("-m", "--imsi", dest="imsi", default="", type="string", help='IMSI to track (default : None, Example: 123456789101112)')
-    (options, args) = parser.parse_args()
-
-    imsi_decrypter = ImsiDecrypter()
-    imsi_decrypter.start()
+    def get_imsi(self, packet):
+        if packet.haslayer(GSM_SMS):
+            gsm_sms = packet.getlayer(GSM_SMS)
+            if hasattr(gsm_sms, "rpdu") and hasattr(gsm_sms.rpdu, "tpdu") and hasattr(gsm_sms.rpdu.tpdu, "tp_ud"):
+                tp_ud = gsm_sms.rpdu.tpdu.tp_ud
+                match = re.findall(r'\b([0-9]{15})\b', tp_ud)
+                if match:
+                    self.imsi = match[0]
+                    self.filter_imsi()
+        elif packet.haslayer(GSM_RR):
+            gsm_rr = packet.getlayer(GSM_RR)
+            if hasattr(gsm_rr, "channel_type"):
+                if gsm_rr.channel_type == "BCCH":
+                    if hasattr(gsm_rr, "cell_identity"):
+                        self.ci = gsm_rr.cell_identity
+                        self.lac = gsm_rr.location_area_identification
+                        self.mcc = gsm_rr.mobile_country_code
+                        self.mnc = gsm_rr.mobile_network_code
+                elif gsm_rr.channel_type == "AGCH":
+                    if hasattr(gsm_rr, "channel_description"):
+                        channel_description = gsm_rr.channel_description
+                        match = re.findall(r'([0-9A-F]{4,8})\b', channel_description)
+                        if match:
+                            self.tmsi = match[0]
+                            if self.validate_tmsi(self.tmsi):
+                                if options.imsi == '':
+                                    self.filter_imsi()
+                                elif options.imsi == self.imsi:
+                                    self.filter_imsi()
+    elif packet.haslayer(GSM_A_CCCH):
+        gsm_a_ccch = packet.getlayer(GSM_A_CCCH)
+        if hasattr(gsm_a_ccch, "channel_type"):
+            if gsm_a_ccch.channel_type == "BCCH":
+                if hasattr(gsm_a_ccch, "gsm_a_bssmap_cell_ci"):
+                    self.ci = gsm_a_ccch.gsm_a_bssmap_cell_ci
+                    self.lac = gsm_a_ccch.gsm_a_lac
+                    self.mcc = gsm_a_ccch.gsm_a_bssmap_plmn_mcc
+                    self.mnc = gsm_a_ccch.gsm_a_bssmap_plmn_mnc
+            elif gsm_a_ccch.channel_type == "SDCCH":
+                if hasattr(gsm_a_ccch, "e212.imsi"):
+                    self.imsi = gsm_a_ccch.e212_imsi
+                    self.tmsi = ""
+                    if self.validate_imsi(self.imsi):
+                        if options.imsi == '':
+                            self.filter_imsi()
+                        elif options.imsi == self.imsi:
+                            self.filter_imsi()
+                elif hasattr(gsm_a_ccch, "gsm_a_rr_tmsi_ptmsi"):
+                    self.tmsi = gsm_a_ccch.gsm_a_rr_tmsi_ptmsi
+                    if self.validate_tmsi(self.tmsi):
+                        if options.imsi == '':
+                            self.filter_imsi()
+                        elif options.imsi == self.imsi:
+                            self.filter_imsi()
